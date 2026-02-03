7dcbff63ddc240e483c0d8dc77ecb2f92397e10b
Haujet Zhao <1292756898@qq.com>
2026-02-04 01:02:51 +0800

    encoder 换用 fp16，在 dml 加速下，300秒的音频可以9.9秒转录完；60秒的音频可以1.9秒转录完。对于普通音频，RTF可以达到0.03以下。

--- 04-Inference.py
+++ 04-Inference.py
@@ -35,8 +35,8 @@ json_output = False
 
 # 模型文件路径
 model_dir = "./model"
-encoder_onnx_path = f"{model_dir}/Fun-ASR-Nano-Encoder-Adaptor.fp32.onnx"
-ctc_onnx_path = f"{model_dir}/Fun-ASR-Nano-CTC.fp32.onnx"
+encoder_onnx_path = f"{model_dir}/Fun-ASR-Nano-Encoder-Adaptor.fp16.onnx"
+ctc_onnx_path = f"{model_dir}/Fun-ASR-Nano-CTC.fp16.onnx"
 decoder_gguf_path = f"{model_dir}/Fun-ASR-Nano-Decoder.q8_0.gguf"
 tokens_path = f"{model_dir}/tokens.txt"
 hotwords_path = "./hot.txt"  # 可选，留空则不使用热词
@@ -75,13 +75,13 @@ def main():
         verbose=verbose,
     )
 
-    print(f'\n预跑一遍，分配内存......\n')
+    input(f'\n预跑一遍，分配内存......\n')
     result = engine.transcribe(
         audio_file, 
         language=language, 
         context=context, 
         verbose=False, 
-        duration=60.0,
+        duration=5.0,
     )
 
     input('回车开始转录......')
